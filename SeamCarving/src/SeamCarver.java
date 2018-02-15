5f4acddbf4529e1b42fadce04c6e3dbb76ac8885
bhscs-build <bhs.cs.build@gmail.com>
2018-02-15 11:43:28 -0800

    Populating starter project: Part 2

---
+++ SeamCarving/src/SeamCarver.java
@@ -0,0 +1,46 @@
+public class SeamCarver
+{
+	public SeamCarver(SmC_Picture pictureP)
+	{
+	}
+
+	public SmC_Picture picture()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public int width()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public int height()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public double energy(int x, int y)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public int[] findHorizontalSeam()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public int[] findVerticalSeam()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public void removeHorizontalSeam(int[] a)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	public void removeVerticalSeam(int[] a)
+	{
+		throw new UnsupportedOperationException();
+	}
+}
