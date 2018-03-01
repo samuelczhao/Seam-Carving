e4935eecc5f12d4a344c5f1a14413ecde39b92a9
s-zhaos <s-zhaos@51L104-08.it.bsd405.org>
2018-03-01 10:50:17 -0800

    kappa

--- SeamCarving/src/SeamCarver.java
+++ SeamCarving/src/SeamCarver.java
@@ -1,27 +1,55 @@
+import java.awt.Color;
+
 public class SeamCarver
 {
+	private SmC_Picture p;
+	private SmC_Picture curr;
+	
 	public SeamCarver(SmC_Picture pictureP)
 	{
+		p = pictureP;
+		curr = pictureP;
 	}
 
 	public SmC_Picture picture()
 	{
-		throw new UnsupportedOperationException();
+		return curr;
 	}
 
 	public int width()
 	{
-		throw new UnsupportedOperationException();
+		return p.width();
 	}
 
 	public int height()
 	{
-		throw new UnsupportedOperationException();
+		return p.height();
 	}
 
 	public double energy(int x, int y)
 	{
-		throw new UnsupportedOperationException();
+		if (x == 0 || y == 0 || x == width() || y == height())
+		{
+			return 1000;
+		}
+		
+		Color w = curr.get(x, y - 1);
+		Color a = curr.get(x - 1, y);
+		Color s = curr.get(x, y + 1);
+		Color d = curr.get(x + 1, y);
+		
+		double vR = w.getRed() - s.getRed();
+		double vG = w.getGreen() - s.getGreen();
+		double vB = w.getBlue() - s.getBlue();
+		
+		double hR = a.getRed() - d.getRed();
+		double hG = a.getGreen() - d.getGreen();
+		double hB = a.getBlue() - d.getBlue();
+		
+		double p1 = (vR * vR) + (vG * vG) + (vB * vB);
+		double p2 = (hR * hR) + (hG * hG) + (hB * hB);
+		
+		return Math.sqrt(p1 + p2);
 	}
 
 	public int[] findHorizontalSeam()
